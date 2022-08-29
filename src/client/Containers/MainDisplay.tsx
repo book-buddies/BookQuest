c4b35b02202dca1848b04fd6decea19c0db90694
fried-jo <101134988+fried-jo@users.noreply.github.com>
2022-08-29 19:22:48 -0400

    If merged this commit begins implementing context for state management

--- src/client/Containers/MainDisplay.tsx
+++ src/client/Containers/MainDisplay.tsx
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { render } from 'react-dom';
 import ResultsContainer from './ResultsContainer'
 import NavBar from './NavBar'
 
+
+interface ContextState {
+  name: string | null;
+}
+// const BookContext = createContext <string | null>(null);
+const BookContext = createContext({} as ContextState);
+
+const [searchResults, setSearchResults] = useState('')
+
 export default function MainDisplay() {
 
-const [query, setQuery] = useState('')
 
-    function updateResults(){
-      console.log('hi');
-      setQuery('hi');
-    }
-    return (
-      <>
-        <NavBar/>
-        <ResultsContainer/>
-      </>
-    );
+  function updateResults(){
+    console.log('hi');
+    // setQuery('hi');
   }
+  return (
+    <BookContext.Provider value={searchResults}>
+      <NavBar/>
+      <ResultsContainer/>
+    </BookContext.Provider>
+  );
+}
 
--- src/client/Containers/NavBar.tsx
+++ src/client/Containers/NavBar.tsx
@@ -1,30 +1,36 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import React from 'react';
 import { render } from 'react-dom';
+import BookContext from './MainDisplay'
 
 export default function NavBar() {
+  const query = useContext(BookContext)
 
-    function handleSubmit(e: React.SyntheticEvent<EventTarget>){
-      //for forms, use preventDefault to prevent submitting from automatically refreshing the page
-      e.preventDefault();
-      console.log('stop clicking me you jerk!');
-      
-    }
-  
-    return (
-      <>
-      <h1>BookQuest</h1>
-      <h2>By BookBuddies</h2>
-        <form action="">
-          <input
-          type='text'
-          name='bookSearch'
-          id='bookSearch'
-          placeholder='title/author or ISBN'
-          required
-          />
-          <input type='submit' name='submit' value='submit' onClick={handleSubmit}/>
-        </form>
-      </>
-    );
+  function handleSubmit(e: React.SyntheticEvent<EventTarget>){
+    //for forms, use preventDefault to prevent submitting from automatically refreshing the page
+    e.preventDefault();
+    console.log('stop clicking me you jerk!');
+    const bookSearch = document.getElementById('bookSearch');
+    console.log(bookSearch)
+    //fetch request to backend with booksearch parameters
+    //get object with results back
+    //query.setSearchResults(data)
   }
+
+  return (
+    <div className = 'nav'>
+    <h1>BookQuest</h1>
+    <h2>By BookBuddies</h2>
+      <form action="">
+        <input
+        type='text'
+        name='bookSearch'
+        id='bookSearch'
+        placeholder='title/author or ISBN'
+        required
+        />
+        <input type='submit' name='submit' value='submit' onClick={handleSubmit}/>
+      </form>
+    </div>
+  );
+}
